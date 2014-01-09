d9c70195e1200ad3a17a2579c7548a3e77bb228c
Wouter van Drunen <wvandrunen@gmail.com>
2014-01-09 16:17:34 +0100

    example

--- src/main/java/com/quartz/example/App.java
+++ src/main/java/com/quartz/example/App.java
@@ -1,9 +1,48 @@
 package com.quartz.example;
 
-public class App 
+import org.quartz.*;
+import org.quartz.impl.JobDetailImpl;
+import org.quartz.impl.StdSchedulerFactory;
+
+import static org.quartz.DateBuilder.futureDate;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SchedulerException {
+
+
+        JobDetail job = newJob(ExampleJob.class)
+                            .withIdentity("example job", "example group")
+                            .build();
+
+        Trigger trigger = newTrigger()
+                                .withIdentity("exampleTrigger", "exampleTriggerGroup")
+                                .startNow()
+                                .withSchedule(
+                                        simpleSchedule()
+                                                .withIntervalInMilliseconds(1000)
+                                                .repeatForever()
+                                )
+                                .build();
+
+        SchedulerFactory schFactory = new StdSchedulerFactory();
+
+        Scheduler sch = schFactory.getScheduler();
+
+        sch.start();
+
+        sch.scheduleJob(job, trigger);
     }
+
+    public class ExampleJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+            System.out.println(String.format("Executing current job: {0)", jobExecutionContext));
+        }
+    }
+
 }
